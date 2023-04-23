ad9d68a88dc6ba4f96c8c71047de615ac612ff68
Chareeef <youssef.charif.h@gmail.com>
2023-04-23 13:42:48 +0000

    _putchar description

--- _putchar.c
+++ _putchar.c
@@ -4,8 +4,7 @@
  * _putchar - writes the character c to stdout
  * @c: The character to print
  *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
+ * Return: On success 1, -1 on error.
  */
 
 int	_putchar(char c)
