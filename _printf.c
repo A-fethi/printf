e532f8c150fbf6f6d60f4da80b3cf6da0d5d740b
Chareeef <youssef.charif.h@gmail.com>
2023-04-23 17:16:15 +0000

    binary

--- _printf.c
+++ _printf.c
@@ -22,6 +22,8 @@ void	_if(va_list args, char type, int *len)
 		(*len) = (*len) + _puts(va_arg(args, char *));
 	else if (type == 'd' || type == 'i')
 		print_num(va_arg(args, int), len);
+	else if (type == 'b')
+		print_binary(va_arg(args, int), len);
 	else
 	{
 		(*len) = (*len) + _putchar('%');
