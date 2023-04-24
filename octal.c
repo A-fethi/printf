9bc6e4e28d2de952e1c02f2fb6260668b96732f5
A-fethi <fethiabderrahmane1@gmail.com>
2023-04-24 07:41:33 -0700

    octal

---
+++ octal.c
@@ -0,0 +1,49 @@
+#include "main.h"
+
+/**
+ * _octal - prints octal number
+ * @num: the number
+ * Return: lenght of number printed
+ */
+
+int	_octal(unsigned int num)
+{
+	int		remainder;
+	int		i;
+	int		len;
+	int		*store;
+	unsigned int	temp;
+
+	i = 0;
+	len = 0;
+	temp = num;
+	while (temp / 8 != 0)
+	{
+		temp = temp / 8;
+		len++;
+	}
+	len++;
+	store = malloc(len * sizeof(int));
+	while (i < len)
+	{
+		remainder = num % 8;
+		store[i++] = remainder;
+		num = num / 8;
+	}
+	i = 0;
+	while (i < len / 2)
+	{
+		temp = store[i];
+		store[i] = store[(len - i - 1)];
+		store[(len - i - 1)] = temp;
+		i++;
+	}
+	i = 0;
+	while (i < len)
+	{
+		_putchar(store[i] + '0');
+		i++;
+	}
+	free(store);
+	return (len);
+}
