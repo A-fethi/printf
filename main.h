b00819d57e6816fc1e316d354c259dd71a599769
A-fethi <fethiabderrahmane1@gmail.com>
2023-04-24 07:41:45 -0700

    HEADER_FILE

--- main.h
+++ main.h
@@ -19,5 +19,6 @@ void	_hexaupper(unsigned long num, int *len);
 void	_unsigned(unsigned long n, int *len);
 int	_non_printable(char *s);
 void	char_to_upper_hex(int num, int *len);
+int	_octal(unsigned int num);
 
 #endif
