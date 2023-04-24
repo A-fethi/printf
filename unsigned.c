d09eabc5a2f7b1839006fc871e9175135d0b4e0c
A-fethi <fethiabderrahmane1@gmail.com>
2023-04-24 03:32:09 -0700

    unsigned

---
+++ unsigned.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * _unsigned - prints a number
+ * @n: our number
+ * @len: pointer to the string length
+ */
+
+void	_unsigned(unsigned long n, int *len)
+{
+	int d = 1000000000, l = 0;
+
+	while (l == 0 && n > 0)
+	{
+		l = (n / d) % 10;
+		d /= 10;
+	}
+
+	while (d > 0 && n > 0)
+	{
+		_putchar(l + '0');
+		(*len)++;
+		l = (n / d) % 10;
+		d /= 10;
+	}
+	_putchar(n % 10 + '0');
+	(*len)++;
+}
