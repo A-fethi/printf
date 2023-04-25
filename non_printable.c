93756378fb7cde14a3cb8bbf2c0b30b7f02b6554
Chareeef <youssef.charif.h@gmail.com>
2023-04-25 12:05:24 +0100

    %S testest

--- non_printable.c
+++ non_printable.c
@@ -14,10 +14,10 @@ int	char_to_upper_hex(int num, int *len)
 	char *up = "0123456789ABCDEF";
 
 	hex = malloc(size * sizeof(char));
-	if (!hex)
+	if (!hex || !up)
 	{
 		free(hex);
-		return (-1);
+		exit(-1);
 	}
 
 	if (num < 16)
@@ -36,22 +36,21 @@ int	char_to_upper_hex(int num, int *len)
 		}
 	}
 	hex[i] = '\0';
-	rev_string(hex);
-	*len += _putchar(hex[0]);
-	*len += _putchar(hex[1]);
+	*len += _putchar(hex[i - 1]);
+	*len += _putchar(hex[i - 2]);
 	free(hex);
 	return (0);
 }
 
 /**
- * _non_printable - prints a sing with _non_printable charaterss
- * @s: our sing
+ * _non_printable - prints a string with _non_printable charaterss
+ * @s: our string
  *
- * Return: actual sing length
+ * Return: actual string length
  */
 int	_non_printable(char *s)
 {
-	int len;
+	int len, i;
 	int c;
 
 	len = 0;
@@ -60,13 +59,14 @@ int	_non_printable(char *s)
 		write(1, "(null)", 6);
 		return (6);
 	}
-	while (s[len])
+	for (i = 0; s[i] != '\0'; i++)
 	{
-		c = s[len];
+		c = s[i];
 		if ((c > 0 && c < 32) || c >= 127)
 		{
 			len += _printf("\\x");
 			char_to_upper_hex(c, &len);
+			_printf(" (endfunc) ");
 		}
 		else
 		{
